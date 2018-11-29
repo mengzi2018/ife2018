fd16bb2ba816a4a0fc1691e5a9717288eecd8d57
mengzi2018 <1224358149@qq.com>
2018-11-29 21:31:50 +0800

    新提交js作业

---
+++ 零基础学院/第十六天,开始感受JS的乐趣/js/index.js
@@ -0,0 +1,12 @@
+document.getElementById("hobby1").onclick = function () {
+    console.log("爬山")
+}
+document.getElementById("hobby2").onclick = function () {
+    console.log("羽毛球")
+}
+document.getElementById("hobby3").onclick = function () {
+    console.log("电影")
+}
+document.getElementById("resume").onclick = function () {
+    alert("你好!")
+}
